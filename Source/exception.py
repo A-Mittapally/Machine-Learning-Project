dc52c2f09dd1508c40326c418e5bd12852a93fe2
Akshay Mittapally <a_mittapally@ucsb.edu>
2026-03-18 15:54:37 -0700

    Exception And Error Handling

--- Source/exception.py
+++ Source/exception.py
@@ -1,17 +1,48 @@
 import sys 
+import logging
+import os
+from datetime import datetime
 
-def error_message_detail(error, error_detail:sys):
+# -------------------- LOGGER SETUP --------------------
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# -------------------- EXCEPTION HANDLING --------------------
+def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
-    filename = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
+    )
 
     return error_message
 
-    )
 
-Class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail = error_detail) 
-        
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+# -------------------- TEST --------------------
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        error = CustomException(e, sys)
+        logging.error(error)
+        raise error
